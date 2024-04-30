6d0d865d06a31c2075084f05971e3ffe93c6ad49
Charles Gao <1030587461@qq.com>
2024-04-30 23:34:18 +0800

    MHA code finished

--- transformer/attention.py
+++ transformer/attention.py
@@ -37,23 +37,49 @@ class TransformerMultiHeadAttention(nn.Module):
         causal_mask.shape = [seq_len, seq_len]
         """
         batch_size = q.shape[0]
-
+        seq_len = q.shape[1]
         # linear projection of q, k and v, after this q.shape = v.shape = k.shape = [batch_size, seq_len, hidden_size]
         q = self.Wq(q)
         k = self.Wk(k)
         v = self.Wv(v)
 
-        # split the head of q, k and v, use torch.Tensor.reshape may be safer
+        # split the head of q, k and v for parallelism, use torch.Tensor.reshape may be safer
         q = q.view(batch_size, -1, self.n_heads, self.head_dim) # q.shape = [batch_size, seq_len, n_heads, head_dim]
         q = q.transpose(1, 2) # q.shape = [batch_size, n_heads, seq_len, head_dim]
         k = k.view(batch_size, -1, self.n_heads, self.head_dim).transpose(1, 2) # same as above
         v = v.view(batch_size, -1, self.n_heads, self.head_dim).transpose(1, 2) # same as above
         k = k.transpose(-2, -1) # k.shape = [batch_size, n_heads, head_dim, seq_len]
-        attn_score = torch.matmul(q, k) / (self.head_dim ** 0.5) # attn_score.shape = [batch_size, n_heads, seq_len, seq_len]
+        attn_score: torch.Tensor = torch.matmul(q, k) / (self.head_dim ** 0.5) # attn_score.shape = [batch_size, n_heads, seq_len, seq_len]
 
         # padding mask
         if padding_mask is not None:
-            pass
+            # After this padding_mask.shape = [batch_size, n_heads, seq_len, seq_len]
+            padding_mask = padding_mask.unsqueeze(1).unsqueeze(1).repeat(1, self.n_heads, seq_len, 1)
+            # The document for masked_fill is useless
+            attn_score = attn_score.masked_fill(padding_mask, value=float('-inf'))
+        if causal_mask is not None:
+            # After this causal_mask.shape = [batch_size, n_heads, seq_len, seq_len]
+            causal_mask = causal_mask.expand(batch_size, self.n_heads, seq_len, seq_len)
+        
+        # add the causal mask
+        attn_score = attn_score + causal_mask
+
+        # softmax
+        attn_score = F.softmax(attn_score,dim=-1) # do softmax in the lowest dimension
+
+        # 'Retrieve' in the values by matmul with V
+        '''
+        Before:
+        attn_score.shape = [batch_size, n_heads, seq_len, seq_len]
+        v.shape = [batch_size, n_heads, seq_len, head_dim]
+        After:
+        attn_score.shape = [batch_size, n_heads, seq_len, head_dim]
+        '''
+        attn_score = torch.matmul(attn_score, v)
+
+        # Concat the heads
+        attn_score = attn_score.transpose(1, 2).contiguous().view(batch_size, -1, self.hidden_size)
+        return self.Wo(attn_score) # The last projection to generalize the knowledge of heads
 
 
 
